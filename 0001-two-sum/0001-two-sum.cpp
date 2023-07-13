5596040eee7683897d8a03080c655b3a64f9413f
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2023-07-13 14:48:49 +0700

    Time: 11 ms (84.56%), Space: 11.8 MB (10.98%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> umap;
+        for(int i = 0; i < nums.size(); i++) umap[nums[i]] = i;
+        for(int i = 0; i < nums.size(); i++){
+            if(umap.count(target-nums[i]) && umap[target-nums[i]] != i) 
+                return {i, umap[target-nums[i]]};
+        }
+        return {};
+    }
+};
