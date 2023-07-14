c703891cd0e422b163f5c144f40ba4b012213d44
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2023-07-14 14:46:45 +0700

    Time: 8 ms (13.35%), Space: 8.3 MB (14.29%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+     string gcdOfStrings(string str1, string str2) {
+        int n = str1.length();
+        int m = str2.length();
+        
+        if(n < m){
+            return gcdOfStrings(str2, str1);
+        }else if(str1.find(str2) != 0){   
+            return "";
+        }else if(str2 == ""){
+            return str1;
+        }else{
+            cout << str1.substr(m) << " " << str2 << endl;
+            return gcdOfStrings(str1.substr(m), str2);
+        }
+    }
+};
