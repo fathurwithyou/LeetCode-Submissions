632e694d01c92955eb77566ce1a4c6279c76e751
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-13 17:32:22 +0700

    Time: 46 ms (86.44%), Space: 17 MB (62.45%) - LeetHub

---
+++ 1347-minimum-number-of-steps-to-make-two-strings-anagram/1347-minimum-number-of-steps-to-make-two-strings-anagram.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        int word[26] = {0};
+        for(auto i : t)word[i-97]++;
+        for(auto i : s)word[i-97]--;
+        sort(word, word+26);
+        int pos = 0, neg = 0;
+        for(int i = 0; i < 26; i++){
+            if (word[i] < 0) neg -= word[i];
+            if (word[i] > 0) pos += word[i];
+        }
+        int ans = min(pos, neg) + abs(pos-neg);
+        return ans;
+    }
+};
