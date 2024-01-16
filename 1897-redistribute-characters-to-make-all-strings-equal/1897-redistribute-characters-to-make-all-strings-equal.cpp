7cb539c939ec42a8a5698cd70acded022aeac208
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-16 23:43:02 +0700

    Time: 15 ms (80.85%), Space: 11.9 MB (90.09%) - LeetHub

--- 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
+++ 1897-redistribute-characters-to-make-all-strings-equal/1897-redistribute-characters-to-make-all-strings-equal.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     bool makeEqual(vector<string>& words) {
+        int n = words.size();
         int word[26] = {0};
-        for(int i = 0; i < words.size(); i++){
+        for(int i = 0; i < n; i++){
             for(auto x: words[i])
             word[x-'a']++;
         }
         for(int i = 0; i < 26; i++){
-            if(word[i]%words.size()) return false;
+            if(word[i]%n) return false;
         }
         return true;
     }
