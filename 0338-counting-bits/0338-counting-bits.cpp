5faef0b101fb3355a6f3c8f63841d0ba2b6edd83
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-19 01:31:15 +0700

    Time: 0 ms (100.00%), Space: 8 MB (99.55%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> dp(n+1);
+        dp[0] = 0;
+        for(int i = 1; i<=n; i++) dp[i] = i%2 + dp[i/2];
+        return dp;
+    }
+};
