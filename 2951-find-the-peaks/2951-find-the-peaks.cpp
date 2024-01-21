74535c268af150896bcec59fbd6fc99d7685ad8e
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-21 09:42:03 +0700

    Time: 7 ms (53.36%), Space: 23.6 MB (10.18%) - LeetHub

--- 2951-find-the-peaks/2951-find-the-peaks.cpp
+++ 2951-find-the-peaks/2951-find-the-peaks.cpp
@@ -2,8 +2,10 @@ class Solution {
 public:
     vector<int> findPeaks(vector<int>& mountain) {
         vector<int> ans;
-        for(int i = 1; i < mountain.size()-1; i++){
-            if(mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]) ans.push_back(i);
+        mountain.push_back(100);
+        mountain.insert(mountain.begin(),100);
+        for(int i = 1; i < mountain.size()-2; i++){
+            if(mountain[i] > mountain[i-1] && mountain[i] > mountain[i+1]) ans.push_back(i-1);
         }
         return ans;
     }
