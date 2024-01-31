c03c7f7f0d435499b44977c6317c6149f6c14438
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-31 19:10:00 +0700

    Time: 0 ms (100.00%), Space: 9.6 MB (14.96%) - LeetHub

---
+++ 2810-faulty-keyboard/2810-faulty-keyboard.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string finalString(string s) {
+        string res = ""; 
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'i') reverse(res.begin(), res.end());
+            else res += s[i];
+            cout << res << endl;
+        }
+        return res;
+    }
+};
