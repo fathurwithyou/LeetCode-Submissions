c0cd6f9f164179174da9537c421935d330b016bc
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-31 18:39:43 +0700

    Time: 7 ms (59.42%), Space: 20.1 MB (8.44%) - LeetHub

---
+++ 2960-count-tested-devices-after-test-operations/2960-count-tested-devices-after-test-operations.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int countTestedDevices(vector<int>& batteryPercentages) {
+        int ans = 0;
+        for(int i = 0; i < batteryPercentages.size(); i++)
+            if(batteryPercentages[i]-ans > 0) ans++;
+        
+        return ans;
+    }
+};
