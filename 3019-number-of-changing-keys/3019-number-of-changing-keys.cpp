f466b0a54f2235c23fa3d09a1f949dd12a31175c
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-01-31 18:31:43 +0700

    Time: 0 ms (100.00%), Space: 7.8 MB (91.59%) - LeetHub

--- 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
+++ 3019-number-of-changing-keys/3019-number-of-changing-keys.cpp
@@ -3,12 +3,7 @@ public:
     int countKeyChanges(string s) {
         int ans = 0;
         for(int i = 1; i < s.size(); i++){
-            if(s[i] >= 'a' && s[i] <= 'z'){
-                if(s[i-1] != s[i] && s[i]-s[i-1] != 32) ans++;
-            }
-            else{
-                if(s[i-1] != s[i] && s[i-1]-s[i] != 32) ans++;
-            }
+            if(abs(s[i]-s[i-1]) != 32 && abs(s[i]-s[i-1]) != 0)ans++;
         }
         return ans;
     }
