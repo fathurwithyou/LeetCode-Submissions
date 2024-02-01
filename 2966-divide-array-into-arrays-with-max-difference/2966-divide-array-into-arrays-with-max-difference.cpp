78f4505c165e0ff8d7431a1b2de46b8b58aa04fe
fathurwithyou <82654955+fathurwithyou@users.noreply.github.com>
2024-02-01 12:35:59 +0700

    Time: 130 ms (98.89%), Space: 119.5 MB (39.48%) - LeetHub

--- 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
+++ 2966-divide-array-into-arrays-with-max-difference/2966-divide-array-into-arrays-with-max-difference.cpp
@@ -5,8 +5,8 @@ public:
         vector<vector<int>> ans;
         for(int i = 0; i < nums.size(); i+=3){
             if(nums[i+2]-nums[i] > k) return {};
-            else ans.push_back({nums[i], nums[i+1], nums[i+2]});
         }
+        for(int i = 0; i < nums.size(); i+=3) ans.push_back({nums[i], nums[i+1], nums[i+2]});
         return ans;
     }
 };
